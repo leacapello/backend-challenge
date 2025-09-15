1356504f49985ccf5dd24dbce873e212d8e0ffcc
leacapello <leacapello@hotmail.com>
2025-09-15 18:27:08 -0300

    remove code generated

--- src/test/java/ar/com/lcapello/uala/challenge/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package ar.com.lcapello.uala.challenge;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
