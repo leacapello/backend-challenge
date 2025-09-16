b36661a5b2c1092879be4a2ccd041fba4a0b197b
leacapello <leacapello@hotmail.com>
2025-09-16 09:03:31 -0300

    add follower config mapper and handler

---
+++ src/main/java/ar/com/lcapello/uala/challenge/follower/infrastructure/config/FollowerExceptionMapper.java
@@ -0,0 +1,24 @@
+package ar.com.lcapello.uala.challenge.follower.infrastructure.config;
+
+import ar.com.lcapello.uala.challenge.follower.domain.exception.InvalidFollowException;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.Provider;
+import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
+
+import java.util.Map;
+
+@Provider
+public class FollowerExceptionMapper {
+
+    @ServerExceptionMapper
+    public Response handleInvalidFollow(InvalidFollowException ex) {
+        return Response.status(Response.Status.BAD_REQUEST)
+                .entity(Map.of(
+                        "error", "Invalid Follow",
+                        "message", ex.getMessage()
+                ))
+                .build();
+    }
+}
+
+
