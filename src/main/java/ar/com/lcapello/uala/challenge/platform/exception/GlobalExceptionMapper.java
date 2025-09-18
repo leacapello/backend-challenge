6d22a28f583be477380fa5d4e6713e2e90e30275
leacapello <leacapello@hotmail.com>
2025-09-18 09:33:29 -0300

    refactor

--- src/main/java/ar/com/lcapello/uala/challenge/platform/exception/GlobalExceptionMapper.java
+++ src/main/java/ar/com/lcapello/uala/challenge/platform/exception/GlobalExceptionMapper.java
@@ -2,6 +2,7 @@ package ar.com.lcapello.uala.challenge.platform.exception;
 
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
+import org.hibernate.exception.ConstraintViolationException;
 import org.jboss.logging.Logger;
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 import jakarta.ws.rs.NotFoundException;
@@ -13,7 +14,17 @@ public class GlobalExceptionMapper {
     private static final Logger LOG = Logger.getLogger(GlobalExceptionMapper.class);
 
     @ServerExceptionMapper
-    public Response handleException(Exception ex) {
+    public Response handleAnyException(Exception ex) {
+        if (containsCause(ex, ConstraintViolationException.class)) {
+            LOG.error("Constraint violation", ex);
+            return Response.status(Response.Status.CONFLICT)
+                    .entity(Map.of(
+                            "error", "constraint_violation",
+                            "message", "constraint violated"
+                    ))
+                    .build();
+        }
+
         LOG.error("Unhandled exception", ex);
         return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
                 .entity(Map.of(
@@ -32,6 +43,17 @@ public class GlobalExceptionMapper {
                         "message", ex.getMessage()
                 ))
                 .build();
+    }
+
+    private boolean containsCause(Throwable ex, Class<? extends Throwable> type) {
+        Throwable current = ex;
+        while (current != null) {
+            if (type.isInstance(current)) {
+                return true;
+            }
+            current = current.getCause();
+        }
+        return false;
     }
 
 }
